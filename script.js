efb5684de23355228c5827a1e0e06d3a1da1db3a
porokhniava <136180125+porokhniava@users.noreply.github.com>
2024-05-19 19:55:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const images = [
-  "./image1.jpg",
-  "./image2.jpg",
-  "./image3.jpg",
-  "./image4.jpg",
-  "./image5.jpg",
-  "./image6.jpg",
+  "image1.jpg",
+  "image2.jpg",
+  "image3.jpg",
+  "image4.jpg",
+  "image5.jpg",
+  "image6.jpg",
 ];
 
 let currentIndex = 0;
